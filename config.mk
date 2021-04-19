72f33903461630bc4f612bb3b4d2be8cd9baab5b
blackrow67 <dogukanbey42@gmail.com>
2021-04-19 19:24:18 +0000

    Fixed build issues

--- config.mk
+++ config.mk
@@ -16,7 +16,6 @@ VENDOR_PATH := vendor/meizu/flucam
 
 PRODUCT_COPY_FILES += \
 	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/etc,system/etc) \
-	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/priv-app/FluCam) \
 	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/lib/,system/lib) \
         $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/lib64/,system/lib64) \
         $(call find-copy-subdir-files,*,$(VENDOR_PATH)/vendor/lib/,vendor/lib) \
